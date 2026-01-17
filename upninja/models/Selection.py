6e591ebae9ce585f7af83b88fce71a424af9c21e
Romashka8 <seleznyov.romario@mail.ru>
2026-01-17 21:01:25 +0300

    🟨feat: start writing uplift optimization with hyperopt

--- upninja/models/Selection.py
+++ upninja/models/Selection.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 
+from typing import Any, Optional, Union
+
 from hyperopt import fmin, tpe, STATUS_OK, Trials
 
 from sklearn.model_selection import cross_val_score
@@ -10,23 +12,58 @@ from sklearn.model_selection import cross_val_score
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+class UpliftTune:
+    def __init__(
+        self,
+        model_class: Any,
+        data: np.array,
+        target: np.array,
+        treatment: np.array,
+        space: dict,
+        max_evals: int = 5,
+    ):
+        self.model = model_class(**space)
+
+    def scoring_(
+        self,
+        prediction: np.array,
+        treatment: np.array,
+        target: np.array,
+        rate: float = 0.3,
+    ) -> float:
+        """
+        Calculate Uplift Score:
+            - calc top uplift@k score;
+            - try to reach maximum uplift with hyperopt
+        """
+        order = np.argsort(-prediction)
+        treatment_n = int((treatment == 1).sum() * rate)
+        treatment_p = target[order][treatment[order] == 1][:treatment_n].mean()
+        control_n = int((treatment == 0).sum() * rate)
+        control_p = target[order][treatment[order] == 0][:control_n].mean()
+        score = treatment_p - control_p
+        return score
+    
+    # def get_objective_(self, 
+    #                    model: Any,
+    #                    data: np.array,
+    #                    target: np.array,
+    #                    treatment: np.array
+    #                 ):
+    #     predictions = model.predict(data)
+    #     scores = self.scoring_(
+
+    #     )
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 def findBestParams(
-        model_class,
-        data,
-        target,
-        space,
-        cv=3,
-        scoring="roc_auc",
-        max_evals=5):
+    model_class, data, target, space, cv=3, scoring="roc_auc", max_evals=5
+):
     def objective(space):
         clf = model_class(**space)
-        scores = cross_val_score(
-            clf,
-            data,
-            target,
-            cv=cv,
-            scoring=scoring,
-            n_jobs=-1)
+        scores = cross_val_score(clf, data, target, cv=cv, scoring=scoring, n_jobs=-1)
         loss = -scores.mean()
         return {"loss": loss, "status": STATUS_OK}
 
@@ -38,15 +75,13 @@ def findBestParams(
         algo=tpe.suggest,
         max_evals=max_evals,
         trials=trials,
-        rstate=np.random.default_rng(0)
+        rstate=np.random.default_rng(0),
     )
 
     best_params = {k: v for k, v in best.items()}
     best_score = -min(trials.losses())
 
-    return {
-        "best_params": best_params,
-        "best_score": best_score,
-        "trials": trials}
+    return {"best_params": best_params, "best_score": best_score, "trials": trials}
+
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
