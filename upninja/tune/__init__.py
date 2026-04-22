8672158b42a8bfe49d03412860df2af219ff559e
Romashka8 <seleznyov.romario@mail.ru>
2026-04-22 20:41:58 +0300

    🟨feat: updated __init__

--- upninja/tune/__init__.py
+++ upninja/tune/__init__.py
@@ -1,8 +1,23 @@
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-from . import Selection
-from . import Spaces
+from .selection import UpliftTune
+from .spaces import (
+    dt_base_params,
+    dt_hp_space,
+    rf_base_params,
+    rf_hp_space,
+    cb_base_params,
+    cb_hp_space,
+)
 
-__all__ = ["Selection", "Spaces"]
+__all__ = [
+    "UpliftTune",
+    "dt_base_params",
+    "dt_hp_space",
+    "rf_base_params",
+    "rf_hp_space",
+    "cb_base_params",
+    "cb_hp_space",
+]
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
